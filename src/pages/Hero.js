b5028034e4ee19eb8abcd548225e00ef50459c94
Ukeje Patience Ndidiamaka <patienceukeje3006@gmail.com>
2022-10-11 13:02:32 +0100

    Initial commit

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
         <>
           <section id="hero" className="d-flex">
           <div className="container position-relative" data-aos="fade-up" data-aos-delay="100">
-            <div className="row justify-content-left">
+            <div className="row justify-content-left" id="api">
               <div className="col-xl-6 col-lg-6 text-left" id="right-header-content">
                 <p className="text-pre-title">Buypower For Developers</p>
                 <h2 className="text-title">Create a smooth electricity payment experience with Buypower's API</h2>
