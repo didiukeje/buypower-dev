978a213d7b75277b78604b0ad8ca312d10d206e9
Ukeje Patience Ndidiamaka <patienceukeje3006@gmail.com>
2022-10-20 23:19:48 +0100

    Navbar changes

--- src/layout/components/Navbar.js
+++ src/layout/components/Navbar.js
@@ -2,24 +2,18 @@ const Navbar = () => {
     return (
         <>
           <header id="header" className="fixed-top">
-          <div className="container d-flex align-items-center justify-content-between">
+          <div className="container">
 
             <h1 className="logo"><a href="index.html">
-              <img alt="" src={require("../assets/img/buypower-logo.png").default} /></a>
+              <img alt="" src={require("../assets/img/buypower-logo.png")} /></a>
             </h1>
 
-            <nav id="navbar" className="navbar navbar-expand-lg">
-             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
-               <span className="navbar-toggler-icon"></span>
-             </button>
-             <div  className="collapse navbar-collapse" id="navmenu">
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item"><a className="nav-link scrollto" href="/team">FAQs</a></li>
-                <li className="nav-item"><a className="nav-link scrollto pre-btn" href="/contact">Contact us</a></li>
+            <nav id="navbar" className="navbar">
+              <ul>
+                <li className="nav-item"><a className="nav-link" href="/team">FAQs</a></li>
+                <li className="nav-item"><a className="nav-link" href="/contact">Contact us</a></li>
                 <li className="nav-item"><a className="nav-link btn-get-started scrollto" href="/about">Get Started</a></li>
               </ul>
-              <i className="bi bi-list mobile-nav-toggle"></i>
-             </div>
             </nav>
           
 
